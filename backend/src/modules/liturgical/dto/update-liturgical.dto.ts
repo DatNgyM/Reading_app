6a2a01d5c72b05e11d2790f8f72b086368867cbc
baomap456 <126744704+baomap456@users.noreply.github.com>
2025-10-31 19:51:17 +0700

    tách logic và UI

--- backend/src/modules/liturgical/dto/update-liturgical.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLiturgicalDto } from './create-liturgical.dto';
-
-export class UpdateLiturgicalDto extends PartialType(CreateLiturgicalDto) {}
--- backend/src/modules/liturgical/liturgical.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LiturgicalService } from './liturgical.service';
-import { LiturgicalController } from './liturgical.controller';
-
-@Module({
-  controllers: [LiturgicalController],
-  providers: [LiturgicalService],
-})
-export class LiturgicalModule {}
